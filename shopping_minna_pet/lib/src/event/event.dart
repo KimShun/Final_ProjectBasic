0f7a0723a9944aac2a8e42b1fdefa9b3cf64bd75
ShunKim <kimsih0818@gmail.com>
2024-05-22 10:45:14 +0900

    SHA키 공유 - .keystore

---
+++ shopping_minna_pet/lib/src/event/event.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventPageScreen extends StatelessWidget {
+  const EventPageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
