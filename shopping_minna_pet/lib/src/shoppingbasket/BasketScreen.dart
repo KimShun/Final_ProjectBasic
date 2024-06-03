77fa660efe44ddd928a58a81ec2e47000131a19a
yuma2935 <yuma2935@gmail.com>
2024-06-03 22:05:44 +0900

    BasketScreen.dart modify

--- shopping_minna_pet/lib/src/shoppingbasket/BasketScreen.dart
+++ shopping_minna_pet/lib/src/shoppingbasket/BasketScreen.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:dot_navigation_bar/dot_navigation_bar.dart';
-import 'package:shopping_minna_pet/src/common/cubit/navigation_cubit.dart';
-import 'package:shopping_minna_pet/src/common/cubit/authentication_cubit.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:shopping_minna_pet/src/common/component/app_text.dart';
 
 class BasketScreen extends StatefulWidget {
-
   const BasketScreen({Key? key}) : super(key: key);
+
   @override
   _BasketScreenState createState() => _BasketScreenState();
 }
 
-class  _BasketScreenState extends State<BasketScreen> {
+class _BasketScreenState extends State<BasketScreen> {
+
+  final List<CartItemModel> _cartItems = [
+    CartItemModel(imagePath: "assets/app/sale1.png", title: '세최이드레스', price: '30000'),
+    CartItemModel(imagePath: "assets/app/sale2.png", title: '세최이드레스', price: '30000'),
+    CartItemModel(imagePath: "assets/app/sale3.png", title: '세최이드레스', price: '30000'),
+
+  ];
+
+
+  final List<bool> _isCheckedList = [false, false,false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context); //뒤로가기
-            },
-            color: Colors.amber,
-            iconSize: 35,
-            icon: Icon(Icons.arrow_back)),
-        title: Image.asset(
-          "assets/app/logo2.png",
-          width: 150,
-          height: 130,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: Colors.amber,
+          iconSize: 35,
+          icon: Icon(Icons.arrow_back),
+        ),
+        title: const AppText(
+          title: '장바구니',
+          fontSize: 25,
+          color: Colors.brown,
         ),
         centerTitle: true,
         actions: const [
@@ -38,11 +45,60 @@ class  _BasketScreenState extends State<BasketScreen> {
             color: Colors.brown,
             size: 40,
           ),
-
           SizedBox(width: 10.0),
         ],
       ),
-
+      body: Column(
+        children: [
+          Container(
+              height: 2, width: double.maxFinite, color: Colors.black87),
+          const SizedBox(height: 30),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _cartItems.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    IconButton(
+                      onPressed: () => context.push('/saledetail'),
+                      icon: Image.asset(
+                        _cartItems[index].imagePath,
+                        width: 130,
+                        height: 130,
+                      ),
+                    ),
+                    const SizedBox(width: 40.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppText(
+                          title: _cartItems[index].title,
+                          fontSize: 25,
+                          color: Colors.black,
+                        ),
+                        AppText(
+                          title: _cartItems[index].price,
+                          fontSize: 25,
+                          color: Colors.pinkAccent,
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 30.0),
+                    Checkbox(
+                      value: _isCheckedList[index],
+                      onChanged: (value) {
+                        setState(() {
+                          _isCheckedList[index] = value!;
+                        });
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.only(bottom: 20),
         child: Row(
@@ -51,27 +107,26 @@ class  _BasketScreenState extends State<BasketScreen> {
             SizedBox(
               width: 280.0,
               child: ElevatedButton(
-                  onPressed: () {
-                    // 로그인 버튼 클릭 시 동작을 여기에 추가합니다.
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 3, 199, 90),
-                    surfaceTintColor: const Color.fromARGB(255, 3, 199, 90),
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10.0, horizontal: 30),
-                    textStyle: const TextStyle(fontSize: 20),
-                    //primary: Colors.green,
+                onPressed: () {
+
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 3, 199, 90),
+                  surfaceTintColor: const Color.fromARGB(255, 3, 199, 90),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  child: const AppText(
-                    title: "주문하기",
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  )
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 30),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                child: const AppText(
+                  title: "주문하기",
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
           ],
@@ -79,4 +134,16 @@ class  _BasketScreenState extends State<BasketScreen> {
       ),
     );
   }
+}
+
+class CartItemModel {
+  final String title;
+  final String price;
+  final String imagePath;
+
+  CartItemModel({
+    required this.title,
+    required this.price,
+    required this.imagePath,
+  });
 }
